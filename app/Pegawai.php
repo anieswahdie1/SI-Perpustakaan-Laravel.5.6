d52402a47604b670af4ac854109059ad91235f42
anieswahdie1 <manies5396@gmail.com>
2020-09-23 10:58:22 +0700

    Menambahkan PegawaiController, routes, view pegawai dan auth

--- app/Pegawai.php
+++ app/Pegawai.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Pegawai extends Model
 {
     protected $table = 'pegawais';
+    protected $fillable = [
+        'id',
+        'id_pegawai',
+        'nama',
+        'jenis_kelamin',
+        'no_hp',
+        'alamat'
+    ];
 }
---
+++ resources/views/pegawai/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('content')
+    <table class="table table-striped">
+        <tr>
+            <th>Id Pegawai</th>
+            <th>Nama</th>
+            <th>Jenis Kelamin</th>
+            <th>No HP</th>
+            <th>Alamat</th>
+        </tr>
+        @foreach ($datas as $data)
+            <tr>
+                <td>{{ $data->id_pegawai }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->jenis_kelamin }}</td>
+                <td>{{ $data->no_hp }}</td>
+                <td>{{ $data->alamat }}</td>
+            </tr>    
+        @endforeach
+        
+    </table>
+
+@endsection
