e535b855fd98244e7f16aefd1058691b2380c2d7
anieswahdie1 <manies5396@gmail.com>
2020-09-23 14:40:37 +0700

    membuat fungsi tambah data pada pegawai

--- resources/views/pegawai/index.blade.php
+++ resources/views/pegawai/index.blade.php
@@ -1,23 +1,36 @@
 @extends('layouts.app')
 @section('content')
-    <table class="table table-striped">
-        <tr>
-            <th>Id Pegawai</th>
-            <th>Nama</th>
-            <th>Jenis Kelamin</th>
-            <th>No HP</th>
-            <th>Alamat</th>
-        </tr>
-        @foreach ($datas as $data)
-            <tr>
-                <td>{{ $data->id_pegawai }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->jenis_kelamin }}</td>
-                <td>{{ $data->no_hp }}</td>
-                <td>{{ $data->alamat }}</td>
-            </tr>    
-        @endforeach
+
+<div class="row">
+    <div class="col-md-10">
+        <div>
+            <a href="{{ route('pegawai.create') }}" class="btn-sm btn-danger"> Tambah Data Pegawai</a>
+        </div>
         
-    </table>
+        <div class="card">
+            <div class="card-header"> Data Pegawai </div>
+            <div class="card-body">
+                <table class="table table-striped">
+                    <tr>
+                        <th>Id Pegawai</th>
+                        <th>Nama</th>
+                        <th>Jenis Kelamin</th>
+                        <th>No HP</th>
+                        <th>Alamat</th>
+                    </tr>
+                    @foreach ($datas as $data)
+                        <tr>
+                            <td>{{ $data->id_pegawai }}</td>
+                            <td>{{ $data->nama }}</td>
+                            <td>{{ $data->jenis_kelamin }}</td>
+                            <td>{{ $data->no_hp }}</td>
+                            <td>{{ $data->alamat }}</td>
+                        </tr>    
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
---
+++ resources/views/pegawai/tambah.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="row">
+    <div class="col-md-10">
+        <div class="card">
+            <div class="card-header"> Input Data Pegawai </div>
+            <div class="card-body">
+                <form action="{{ route('pegawai.store') }}" method="POST">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="id_pegawai">ID Pegawai</label>
+                        <input type="text" name="id_pegawai" class="form-control" id="id_pegawai" placeholder="ID Pegawai">
+                    </div>
+                    <div class="form-group">
+                        <label for="nama">Nama Pegawai</label>
+                        <input type="text" name="nama" class="form-control" id="nama" placeholder="Nama Pegawai">
+                    </div>
+                    <div class="form-group">
+                        <label for="jenis_kelamin">Jenis Kelamin</label>
+                        <select name="jenis_kelamin" id="jenis_kelamin" class="form-control" >
+                            <option value="">-- Pilih --</option>
+                            <option value="Pria">Pria</option>
+                            <option value="Wanita">Wanita</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="no_hp">Nomor HP</label>
+                        <input type="number" name="no_hp" class="form-control" id="no_hp" placeholder="Nomor HP">
+                    </div>
+                    <div class="form-group">
+                        <label for="alamat">Alamat</label>
+                        <input type="text" name="alamat" class="form-control" id="alamat" placeholder="Alamat">
+                    </div>
+                    
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
